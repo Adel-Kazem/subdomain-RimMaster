3e9e56b8804ff92f68f8fec678b89ddbadc16f92
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-03-26 14:36:45 +0200

    Add new product

--- products.js
+++ products.js
@@ -58,5 +58,17 @@ const PRODUCTS = [
       "https://raw.githubusercontent.com/Adel-Kazem/subdomain-RimMaster/main/products_images/Allen-Edmonds-Newland-Ave-dresscasual-belt.jpg"
     ],
     rating: 5
+  },
+  {
+    id: 9,
+    name: "testtttttttttt",
+    description: "Nisi ex dolores occa",
+    price: 42,
+    images: [
+      "https://raw.githubusercontent.com/Adel-Kazem/subdomain-RimMaster/main/products_images/Allen-Edmonds-Newland-Ave-dresscasual-belt.jpg",
+      "https://raw.githubusercontent.com/Adel-Kazem/subdomain-RimMaster/main/products_images/Belts-Style-Image-3.jpg",
+      "https://raw.githubusercontent.com/Adel-Kazem/subdomain-RimMaster/main/products_images/Belts-Style-Image-4.jpg"
+    ],
+    rating: 5
   }
 ];
